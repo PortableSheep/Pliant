639d6cd4f71b175e3fbf0bee3dcbda4939c4556e
Michael Gunderson <admin@rushtheweb.com>
2012-12-02 20:39:08 -0800

    Refactored decorator to work automatically.
    Decorator plugin will now auto detect decorations based on rule names,
    and decorate the field.

--- plugins/pl.decorator.js
+++ plugins/pl.decorator.js
@@ -1,56 +1,50 @@
-/*! v1.0 */
+/*! v1.1 */
 (function($) {
 	$.pliantPlugins.decorator = function(options, plInst) {
         var opt = $.extend({}, options);
         plInst.Subscribe('onFieldAdded', function(fieldObj) {
-            if (fieldObj.decorate) {
-                if (fieldObj.decorate.rule && !fieldObj.rules[fieldObj.decorate.rule]) {
-                    return;
-                }
-                if (typeof(fieldObj.decorate) === 'string' && opt.items[fieldObj.decorate]) {
-                    fieldObj.decorate = $.extend({}, opt.items[fieldObj.decorate]);
-                }
-                fieldObj.decorate.content = (fieldObj.decorate.content instanceof jQuery ? fieldObj.decorate.content.clone() : $(fieldObj.decorate.content));
-                var what = (fieldObj.decorate.what instanceof Function ? fieldObj.decorate.what.call(this) : fieldObj.decorate.what);
-
-                switch(fieldObj.decorate.how.toLowerCase()) {
-                    case 'prepend':
-                        what.prepend(fieldObj.decorate.content);
-                    break;
-                    case 'append':
-                        what.append(fieldObj.decorate.content);
-                    break;
-                    case 'before':
-                        what.before(fieldObj.decorate.content);
-                    break;
-                    case 'after':
-                        what.after(fieldObj.decorate.content);
-                    break;
-                    default:
-                    break;
-                }
-            }
-        }).Subscribe('onFieldRemoved', function() {
-            if (this.decorate) {
-                this.decorate.content.remove();
-            }
-        }).Subscribe('onFieldToggle', function(isEnabled) {
-            if (this.decorate && this.decorate.sync) {
-                this.decorate.content.toggle(isEnabled);
-            }
-        }).Subscribe('onFieldRuleToggle', function() {
-            if (this.decorate && this.decorate.sync) {
-                var hasEnabled = false;
-                for(var i in this.rules) {
-                    if (this.rules[i].isEnabled === undefined || this.rules[i].isEnabled === true) {
-                        if (this.decorate.rule && this.decorate.rule !== i) {
-                            continue;
-                        }
-                        hasEnabled = true;
+            for(var i in fieldObj.rules) {
+                if (opt.hasOwnProperty(i)) {
+                    fieldObj.rules[i].decorate = $.extend({}, opt[i]);
+                    if (!(fieldObj.rules[i].decorate.content instanceof jQuery)) {
+                        fieldObj.rules[i].decorate.content = $(fieldObj.rules[i].decorate.content);
+                    }
+                    var what = ($.isFunction(fieldObj.rules[i].decorate.what) ? fieldObj.rules[i].decorate.what.call(this) : fieldObj.rules[i].decorate.what);
+                    switch(fieldObj.rules[i].decorate.how.toLowerCase()) {
+                        case 'prepend':
+                            what.prepend(fieldObj.rules[i].decorate.content);
+                        break;
+                        case 'append':
+                            what.append(fieldObj.rules[i].decorate.content);
+                        break;
+                        case 'before':
+                            what.before(fieldObj.rules[i].decorate.content);
+                        break;
+                        case 'after':
+                            what.after(fieldObj.rules[i].decorate.content);
+                        break;
+                        default:
                         break;
                     }
                 }
-                this.decorate.content.toggle(hasEnabled);
+            }
+        }).Subscribe('onFieldRemoved', function() {
+            for(var i in this.rules) {
+                if (this.rules[i].decorate) {
+                    this.rules[i].decorate.content.remove();
+                }
+            }
+        }).Subscribe('onFieldToggle', function(isEnabled) {
+            for(var i in this.rules) {
+                if (this.rules[i].decorate && this.rules[i].decorate.sync) {
+                    this.rules[i].decorate.content.toggle(isEnabled);
+                }
+            }
+        }).Subscribe('onFieldRuleToggle', function(isEnabled, name) {
+            for(var i in this.rules) {
+                if (name == i && this.rules[i].decorate && this.rules[i].decorate.sync) {
+                    this.rules[i].decorate.content.toggle(isEnabled);
+                }
             }
         });
     };
